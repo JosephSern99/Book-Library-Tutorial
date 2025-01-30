d47f15cde5f3df0187a9fc7833497d7cd96499b2
josephkhoo99 <joseph@djavafactory.com>
2025-01-31 02:49:19 +0800

    Login and Database Setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,25 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import { sampleBooks } from "../constants";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 
-const Home = () => (
-  <>
-    <BookOverview {...sampleBooks[0]} />
-    <BookList
-      title="Latest Books"
-      books={sampleBooks}
-      containerClassName="mt-28"
-    />
-  </>
-);
+const Home = async () => {
+  // database connection to drizzle
+
+  const result = await db.select().from(users);
+  console.log(JSON.stringify(result));
+
+  return (
+    <>
+      <BookOverview {...sampleBooks[0]} />
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
+    </>
+  );
+};
+
 export default Home;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,49 +3,44 @@ import "./globals.css";
 
 import localFont from "next/font/local";
 import { ReactNode } from "react";
+import { Toaster } from "@/components/ui/toaster";
 
+// Setting up two Fonts from the local directory using classname and variable
+const ibmPlexSans = localFont({
+  src: [
+    {
+      path: "./fonts/IBMPlexSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/IBMPlexSans-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/IBMPlexSans-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./fonts/IBMPlexSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
 
-// Setting up two Fonts from the local directory using classname and variable 
-const ibmPlexSans = localFont(
-  {
-    src: [
-      {
-        path: "./fonts/IBMPlexSans-Regular.ttf",
-        weight: "400",
-        style: "normal",
-      },
-      {
-        path: "./fonts/IBMPlexSans-Medium.ttf",
-        weight: "500",
-        style: "normal",  
-      },    
-      {
-        path: "./fonts/IBMPlexSans-SemiBold.ttf",
-        weight: "600",
-        style: "normal",
-      },
-      {
-        path: "./fonts/IBMPlexSans-Bold.ttf",
-        weight: "700",
-        style: "normal",
-      }
-    ],
-  }
-);
-
-const bebasNeue = localFont(
-  {
-    src: [
-      {
-        path: "./fonts/BebasNeue-Regular.ttf",
-        weight: "400",
-        style: "normal",
-      },
-    ],
-    variable: "--bebas-neue",
-  }
-);
-
+const bebasNeue = localFont({
+  src: [
+    {
+      path: "./fonts/BebasNeue-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--bebas-neue",
+});
 
 export const metadata: Metadata = {
   title: "BookWise",
@@ -59,9 +54,10 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
         className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
-}
+};
 
-export default RootLayout
+export default RootLayout;
