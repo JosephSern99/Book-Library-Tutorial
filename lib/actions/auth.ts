243ea38ba149b9163b7102ce1a7732184e6ff4c5
josephkhoo99 <joseph@djavafactory.com>
2025-02-01 17:14:32 +0800

    implement qstash workflows for user onboarding

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -9,6 +9,7 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import config from "@/lib/config";
 import ratelimit from "../ratelimit";
+import { workflowClient } from "../workflow";
 
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, "email" | "password">
@@ -69,13 +70,13 @@ export const signUp = async (params: AuthCredentials) => {
       universityCard,
     });
 
-    //   await workflowClient.trigger({
-    //     url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
-    //     body: {
-    //       email,
-    //       fullName,
-    //     },
-    //   });
+    await workflowClient.trigger({
+      url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
+      body: {
+        email,
+        fullName,
+      },
+    });
 
     await signInWithCredentials({ email, password });
 
